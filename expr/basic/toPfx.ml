174b46be3424fbf77d33bbbf4d8be197776a0693
Julian PEREZ-RAMIREZ <julian.perez-ramirez@imt-atlantique.net>
2025-03-11 00:08:36 +0100

    adding code for question 5.2

--- expr/basic/toPfx.ml
+++ expr/basic/toPfx.ml
@@ -1,7 +1,22 @@
 open Ast
+open BasicPfx.Ast
 
-let generate = function
-  | Const _ -> failwith "To implement"
-  | Binop(_,_,_) -> failwith "To implement"
-  | Uminus _ -> failwith "To implement"
-  | Var _ -> failwith "Not yet supported"
+(* Question 5.2 *)
+let rec generate (expr : expression) : command list =
+  match expr with
+  | Const n -> [Push n]
+  | Binop (op, e1, e2) ->
+    let code1 = generate e1 in  
+    let code2 = generate e2 in  
+    let op_code = match op with  
+      | BinOp.Badd -> Add
+      | BinOp.Bsub -> Sub
+      | BinOp.Bmul -> Mul
+      | BinOp.Bdiv -> Div
+      | BinOp.Bmod -> Rem
+    in
+    code1 @ code2 @ [op_code]  
+  | Uminus e ->
+    let code = generate e in  
+    code @ [Push 0; Sub]  
+  | Var _ -> failwith "Not yet supported" 
