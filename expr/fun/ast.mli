6b51835111adc8187c1e8e1190631ce5c9d87854
Julian PEREZ-RAMIREZ <julian.perez-ramirez@imt-atlantique.net>
2025-03-18 21:56:41 +0100

    adding test for new 3 instructions

--- expr/fun/ast.mli
+++ expr/fun/ast.mli
@@ -1,6 +1,6 @@
 (* The expression type *)
 type expression =
-    Const of int
+  |  Const of int
   | Var of string
   | Binop of BinOp.t * expression * expression
   | Uminus of expression
--- expr/fun/tests/test_generate.ml
+++ expr/fun/tests/test_generate.ml
@@ -1,22 +1,49 @@
 open OUnit2
-open BasicExpr.ToPfx
+open FunExpr.ToPfx
 open BasicPfx.Ast
-open BasicExpr.Ast
+open FunExpr.Ast
+open FunExpr.Eval
 
+(* Test expression: 2 + (3 * 4) *)
 let expr = Binop (BinOp.Badd, Const 2, Binop (BinOp.Bmul, Const 3, Const 4))
-let pfx_code = generate expr
+let pfx_code = generate [] expr
+let result = eval [] expr
 
-let step_tests =
+(* Test expression: (λx.x + 1) 2 *)
+let expr2 = App(Fun("x", Binop(Badd, Var "x", Const 1)), Const 2)
+
+(* Generate Pfx code from the expression *)
+let pfx_code2 = generate [] expr2
+let result2 = eval [] expr2
+
+let generate_tests =
   [
     "Generated code for expression" >:: (fun _ -> 
       let expected = [Push 2; Push 3; Push 4; Mul; Add] in
       assert_equal expected pfx_code
+    );
+    
+    "Generated code for (λx.x + 1) 2" >:: (fun _ -> 
+      let expected = [Push 2; ExecSeq [Push 0; Get; Push 1; Add]; Exec] in
+      assert_equal expected pfx_code2
+    ); 
+  ]
+let eval_tests =
+  [
+    "Evaluate expression" >:: (fun _ -> 
+      let expected = Int 14 in
+      assert_equal expected result
+    ); 
+    "Evaluate expression" >:: (fun _ -> 
+      let expected = Int 3 in
+      assert_equal expected result2
     ); 
   ]
 let suite =
   "Eval Tests"
   >::: [
-         "Step Tests" >::: step_tests;
+         "Generate Tests" >::: generate_tests;
+         "Eval Tests" >::: eval_tests;
        ]
 
 let () = run_test_tt_main suite
