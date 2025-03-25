d274ff2c1990d79d1ab14f0d683aa3c266c886b6
Julian Perez Ramirez <jperez.julian13@gmail.com>
2025-03-25 14:38:41 +0100

    updating the toPfx.ml module and eval.ml module for exercise 5

--- expr/basic/toPfx.ml
+++ expr/basic/toPfx.ml
@@ -5,18 +5,27 @@ open BasicPfx.Ast
 let rec generate (expr : expression) : command list =
   match expr with
   | Const n -> [Push n]
-  | Binop (op, e1, e2) ->
-    let code1 = generate e1 in  
-    let code2 = generate e2 in  
-    let op_code = match op with  
-      | BinOp.Badd -> Add
-      | BinOp.Bsub -> Sub
-      | BinOp.Bmul -> Mul
-      | BinOp.Bdiv -> Div
-      | BinOp.Bmod -> Rem
-    in
-    code1 @ code2 @ [op_code]  
+  | Binop (BinOp.Badd, e1, e2) ->
+      let code1 = generate e1 in  
+      let code2 = generate e2 in  
+      code1 @ code2 @ [Add]
+  | Binop (BinOp.Bsub, e1, e2) ->
+      let code1 = generate e1 in  
+      let code2 = generate e2 in  
+      code2 @ code1 @ [Sub] (* Reverse order *)
+  | Binop (BinOp.Bmul, e1, e2) ->
+      let code1 = generate e1 in  
+      let code2 = generate e2 in  
+      code1 @ code2 @ [Mul]
+  | Binop (BinOp.Bdiv, e1, e2) ->
+      let code1 = generate e1 in  
+      let code2 = generate e2 in  
+      code2 @ code1 @ [Div]  (* Reverse order: denominator first *)
+  | Binop (BinOp.Bmod, e1, e2) ->
+      let code1 = generate e1 in  
+      let code2 = generate e2 in  
+      code2 @ code1 @ [Rem]  (* Reverse order: denominator first *)
   | Uminus e ->
-    let code = generate e in  
-    code @ [Push 0; Sub]  
+      let code = generate e in  
+      code @ [Push 0; Sub]  
   | Var _ -> failwith "Not yet supported"
